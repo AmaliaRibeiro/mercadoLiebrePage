a438f2cbd3688a4799c95296a97da2b5077594b2
AmaliaRibeiro <amalita9421@gmail.com>
2023-05-17 00:38:12 -0300

    changes css, html

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.get ('/', (req, res) => {
 });
 
 app.get ('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.sendFile(path.resolve(__dirname, './register.html'));
 });
 
 app.get ('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.sendFile(path.resolve(__dirname, './login.html'));
 });
 
